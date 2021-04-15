e5d30d5d9334eb738b2ad5de616c12dcac2a7a1b
Lucas Barretto e Silva <mohamed28.lucas@gmail.com>
2021-04-15 01:23:02 -0300

    Revert "My components"

--- src/components/nav/nav_item.js
+++
@@ -1,7 +0,0 @@
-import { HTMLBuilder } from "../../libs/html_builder";
-
-export const navItem = (text, link) => {
-    return HTMLBuilder.build("li", "", { class: "nav__item" }, [
-        ["a", text, { href: link }]
-    ]);
-}
