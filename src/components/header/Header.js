0b40ac266a3c1a151f2ed5f2e172b0d73226659e
Lucas <mohamed28.lucas@gmail.com>
2021-04-23 18:37:49 -0300

    Update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,8 +7,8 @@ export default class Header {
 
     render() {
         return Component.build("header", "", { class: "header" },
-            // ["img", "", { src: "./assets/images/gifs/run-gray.gif", alt: "Logo" }],
-            ["video", "", { width: "128", height: "128", controls: "false"  , loop: "loop", autoplay: "autoplay" }, ["source", "", { src: "./assets/images/gifs/run-gray.gif", type: "video/gif" }]],
+            ["img", "", { src: "https://github.com/Mohamed28/mohamed28.github.io/blob/feature/new-layout/src/assets/images/gifs/run.gif?raw=true", alt: "Logo" }],
+            // ["video", "", { width: "128", height: "128", controls: "false"  , loop: "loop", autoplay: "autoplay" }, ["source", "", { src: "https://github.com/Mohamed28/mohamed28.github.io/blob/feature/new-layout/src/assets/video/run.mp4?raw=true", type: "video/mp4" }]],
             ["h1", "Welcome to the Mohamed28's DOME!"]
         )
     }
