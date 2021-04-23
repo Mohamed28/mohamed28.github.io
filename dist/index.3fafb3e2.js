8a065391f5dd373490d23128a59f4530e9b8dade
Lucas <mohamed28.lucas@gmail.com>
2021-04-23 18:45:45 -0300

    Update

--- dist/index.3fafb3e2.js
+++ dist/index.3fafb3e2.js
@@ -516,8 +516,7 @@ class Header {
     }, ["img", "", {
       src: "https://github.com/Mohamed28/mohamed28.github.io/blob/feature/new-layout/src/assets/images/gifs/run.gif?raw=true",
       alt: "Logo"
-    }], // ["video", "", { width: "128", height: "128", controls: "false"  , loop: "loop", autoplay: "autoplay" }, ["source", "", { src: "https://github.com/Mohamed28/mohamed28.github.io/blob/feature/new-layout/src/assets/video/run.mp4?raw=true", type: "video/mp4" }]],
-    ["h1", "Welcome to the Mohamed28's DOME!"]);
+    }], ["h1", "Welcome to the Mohamed28's DOME!"]);
   }
 }
 exports.default = Header;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,7 +8,6 @@ export default class Header {
     render() {
         return Component.build("header", "", { class: "header" },
             ["img", "", { src: "https://github.com/Mohamed28/mohamed28.github.io/blob/feature/new-layout/src/assets/images/gifs/run.gif?raw=true", alt: "Logo" }],
-            // ["video", "", { width: "128", height: "128", controls: "false"  , loop: "loop", autoplay: "autoplay" }, ["source", "", { src: "https://github.com/Mohamed28/mohamed28.github.io/blob/feature/new-layout/src/assets/video/run.mp4?raw=true", type: "video/mp4" }]],
             ["h1", "Welcome to the Mohamed28's DOME!"]
         )
     }
