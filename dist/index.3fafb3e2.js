2fa51278256baac115dc3be10f78985229a9fe13
Lucas <mohamed28.lucas@gmail.com>
2021-04-25 20:46:41 -0300

    More update

--- dist/index.3fafb3e2.js
+++ dist/index.3fafb3e2.js
@@ -529,6 +529,11 @@ class Component {
     let element = document.createElement(tag, options);
     if (content !== "") element.innerText = content;
     if (Object.keys(options).length > 0) {
+      let event = Object.entries(options).filter(option => option[0].match(/on\w*/)).flat();
+      if (event.length > 0) {
+        element.addEventListener(event[0].slice(2), event[1]);
+        delete options[event[0]];
+      }
       Object.keys(options).forEach(key => element.setAttribute(key, options[key]));
     }
     if (children.length > 0) {
--- src/libs/component.js
+++ src/libs/component.js
@@ -5,9 +5,15 @@ export default class Component {
         if (content !== "") element.innerText = content
 
         if (Object.keys(options).length > 0) {
+            let event = Object.entries(options).filter(option => option[0].match(/on\w*/)).flat()
+
+            if (event.length > 0) {
+                element.addEventListener(event[0].slice(2), event[1])
+                delete options[event[0]]
+            }
+
             Object.keys(options).forEach(key => element.setAttribute(key, options[key]))
         }
-
 
         if (children.length > 0) {
             children.forEach(child => {
